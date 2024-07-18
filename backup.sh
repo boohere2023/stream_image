5891d3cbc83957d397cf732674dd1397a1f250a9
boohere2023 <143770773+boohere2023@users.noreply.github.com>
2024-07-18 14:07:24 +0800

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,28 @@
+fi
+
+# Variables
+VOLUMES=("$WEBSITE_LOG" "$WEBSITE_VHOST" "$WEBSITE_MYSQL" "$WEBSITE_DATA")
+HOSTNAME=$(hostname)
+DATE=$(date +%Y%m%d)
+BACKUP_DIR="./stream_image/backup" # Replace with your backup directory
+BUCKET_NAME="stream-data" # Replace with your UpCloud Object Storage bucket name
+
+# Create a backup directory if it doesn't exist
+mkdir -p "$BACKUP_DIR"
+
+for VOLUME in "${VOLUMES[@]}"; do
+  BACKUP_NAME="backup_${DATE}_${HOSTNAME}_$(basename $VOLUME).tar.gz"
+
+  # Check if the volume is set
+  if [ -z "$VOLUME" ]; then
+    echo "Error: Volume name is empty"
+    continue
+  fi
+
+  # Backup the Docker volume
+  docker run --rm -v "$VOLUME":/volume -v "$BACKUP_DIR":/backup alpine \
+    sh -c "tar czf /backup/$BACKUP_NAME -C /volume ."
+
+  # Upload the backup to UpCloud Object Storage
+  s3cmd put "$BACKUP_DIR/$BACKUP_NAME" s3://"$BUCKET_NAME"/"$BACKUP_NAME"
+done
